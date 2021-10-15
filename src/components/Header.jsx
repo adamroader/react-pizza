7ec725e0f211d4f3077fbc756bd8e4a3e6e2fd1e
abror <abror_1116@mail.ru>
2021-10-15 19:17:55 +0500

    4th video completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 import logoSvg from "../assets/img/pizza-logo.svg";
-import Button from './Button'
+import Button from './Button';
 
 
 function Header() {
     return(
         <div className="header">
         <div className="container">
-          <div className="header__logo">
-            <img width="38" src={logoSvg} alt="Pizza logo" />
-            <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
+          <Link to="/">
+            <div className="header__logo">
+              <img width="38" src={logoSvg} alt="Pizza logo" />
+              <div>
+                <h1>React Pizza</h1>
+                <p>самая вкусная пицца во вселенной</p>
+              </div>
             </div>
-          </div>
+          </Link>
           <div className="header__cart">
+            <Link to="cart">
               <Button className="button--cart">
                 <span>520 ₽</span>
                 <div className="button__delimiter"></div>
@@ -48,7 +52,8 @@ function Header() {
                     />
                 </svg>
                 <span>3</span>
-              </Button>     
+              </Button>    
+            </Link> 
           </div>
         </div>
       </div>
